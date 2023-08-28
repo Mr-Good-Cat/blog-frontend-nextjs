7118e7f4b76158adf39c12c94f56b44c7ea2b3aa
ivan.sankov <sii200192@gmail.com>
2023-08-28 12:04:29 +0700

    move breadcrumbs to separate component

--- src/app/(category-and-article)/[mainCategorySlug]/[categorySlug]/[articleSlug]/page.tsx
+++ src/app/(category-and-article)/[mainCategorySlug]/[categorySlug]/[articleSlug]/page.tsx
@@ -1,6 +1,5 @@
 import { article as articleRequest } from "@/api/blog/article.get";
-import Link from "next/link";
-import ChevronRight from "@/components/icons/ChevronRight";
+import Breadcrumbs from "@/components/Breadcrumbs";
 
 export default async function ArticlePage({
   params,
@@ -19,32 +18,8 @@ export default async function ArticlePage({
 
   return (
     <div className="bg-white w-full p-4">
-      <div className="flex overflow-y-auto whitespace-nowrap items-center">
-        <Link
-          href="/"
-          className="underline text-blue-300 hover:text-blue-500 mr-2"
-        >
-          Home
-        </Link>
+      <Breadcrumbs breadcrumbs={article.breadcrumbs} />
 
-        {article.breadcrumbs.map((b) => {
-          return (
-            <>
-              <div className="mx-2 text-gray-400">
-                <ChevronRight />
-              </div>
-
-              <Link
-                href={b.url}
-                key={b.url}
-                className="underline text-blue-300 hover:text-blue-500 mr-2"
-              >
-                {b.title}
-              </Link>
-            </>
-          );
-        })}
-      </div>
       <h3 className="text-center font-semibold text-xl mb-2">
         {article.title}
       </h3>
---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,38 @@
+import { ArticleBreadcrumbs } from "@/api/blog/article.get";
+import Link from "next/link";
+import ChevronRight from "@/components/icons/ChevronRight";
+
+export default function Breadcrumbs({
+  breadcrumbs,
+}: {
+  breadcrumbs: ArticleBreadcrumbs[];
+}) {
+  return (
+    <div className="flex overflow-y-auto whitespace-nowrap items-center">
+      <Link
+        href="/"
+        className="underline text-blue-300 hover:text-blue-500 mr-2"
+      >
+        Home
+      </Link>
+
+      {breadcrumbs.map((b) => {
+        return (
+          <>
+            <div className="mx-2 text-gray-400">
+              <ChevronRight />
+            </div>
+
+            <Link
+              href={b.url}
+              key={b.url}
+              className="underline text-blue-300 hover:text-blue-500 mr-2"
+            >
+              {b.title}
+            </Link>
+          </>
+        );
+      })}
+    </div>
+  );
+}
